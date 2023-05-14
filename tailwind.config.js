8afb4a807baf39a9e61c33f2cbc11a56be44c320
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-05-14 16:28:02 +0200

    ADD: tailwind initialized

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // paths to your templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
