3ecceec04d4e2185d16c1d548ce8c78730526dd5
Pablo-Aviles-Prieto <pablo.aviles.prieto@gmail.com>
2023-07-15 21:03:50 +0200

    wip guessed work message

--- src/components/CanvasBoard/Board.tsx
+++ src/components/CanvasBoard/Board.tsx
@@ -399,10 +399,11 @@ export const Board: FC<Props> = ({ setAwaitPlayersMsg, setGameCancelled }) => {
     // display the msg in the middle of the screen
     socket.on('user guessed', ({ msg }: { msg: string }) => {
       // TODO: Displays msg in the middle of viewport
-      // Maybe add how many points is getting?? (in that case, change it on backend)
+      // Maybe add how many points is getting?? (in that case, change it on backend)!
       console.log('HAS ACERTADO', msg);
     });
 
+    // TODO: Create the routes to join directly to the room
     // TODO: Send an event in some concrete timers, so the backend gives back hints for the word
 
     return () => {
@@ -528,7 +529,7 @@ export const Board: FC<Props> = ({ setAwaitPlayersMsg, setGameCancelled }) => {
     // TODO: Extract the drawing tools into a component
     // TODO: Display a button to start the game (in case is waiting for more players and no one join)
     // TODO: Disable the input when user is in turnScore ???
-    // TODO: Add a restart game button for the owner (it should display a modal to confirm the action)
+    // TODO: Add a restart game button for the owner (it should display a modal to confirm the action)!
     <>
       {gameState.started &&
         gameState.turn !== undefined &&
---
+++ src/components/GuessedWord.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+// TODO: Animate and display the msg from back
+export const GuessedWord: FC = () => {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center'>...</div>
+  );
+};
